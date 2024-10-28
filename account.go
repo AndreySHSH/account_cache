17d4da1c0894ea99e1bc80fe3526a53f93274b6b
andrey_shsh <laptev.andrey@icloud.com>
2024-10-28 13:39:18 +0300

    add: queue

--- account.go
+++ account.go
@@ -13,25 +13,36 @@ type User struct {
 	key  int
 }
 
-type Accounts struct {
-	mu           sync.RWMutex
-	accounts     []User
-	transactions []*sync.Map
-}
-
 type transaction struct {
 	method string
 	amount float64
 }
 
-func Init() Accounts {
-	return Accounts{
+type Tr struct {
+	User  any
+	Score float64
+}
+
+type Engin struct {
+	Queue chan Tr
+
+	accounts     []User
+	transactions []*sync.Map
+}
+
+func Init() *Engin {
+	e := &Engin{
+		Queue: make(chan Tr, 100000),
+
 		accounts:     []User{},
 		transactions: []*sync.Map{},
 	}
+
+	e.readQueue()
+	return e
 }
 
-func (a *Accounts) GetAccountBalance(user any) (*User, float64, error) {
+func (a *Engin) GetAccountBalance(user any) (*User, float64, error) {
 	for _, userAccount := range a.accounts {
 		if userAccount.meta == user {
 			var balance float64
@@ -46,7 +57,7 @@ func (a *Accounts) GetAccountBalance(user any) (*User, float64, error) {
 	return nil, 0, errors.New("account not found")
 }
 
-func (a *Accounts) AddTransaction(user any, score float64) {
+func (a *Engin) AddTransaction(user any, score float64) {
 	transactionId := shortuuid.New()
 	userTransaction := transaction{
 		method: "add",
@@ -73,6 +84,15 @@ func (a *Accounts) AddTransaction(user any, score float64) {
 				key:  keyUserTransactionStore,
 			})
 			break
+		}
+	}
+}
+
+func (a *Engin) readQueue() {
+	for {
+		select {
+		case tr := <-a.Queue:
+			a.AddTransaction(tr.User, tr.Score)
 		}
 	}
 }
